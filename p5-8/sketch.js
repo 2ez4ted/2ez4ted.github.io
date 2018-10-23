151cd14e552cd4e7bf9558e2ffd226b675139a25
2ez4ted <43014088+2ez4ted@users.noreply.github.com>
2018-10-23 13:53:43 -0600

    1023.1

--- p5-8/sketch.js
+++ p5-8/sketch.js
@@ -1,15 +1,10 @@
-// 2d arrat et shyt
-// Matthia Rosenchild
+// Project Title
+// Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let board = [
-  [1, 0, 1, 0],
-  [0, 1, 0, 1],
-  [1, 0, 1, 0],
-  [0, 1, 0, 1]
-];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
