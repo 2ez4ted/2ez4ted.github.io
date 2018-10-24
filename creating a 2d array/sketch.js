37925e0a7bc0bb6c68d2121c89e1ac13ec02bc94
2ez4ted <43014088+2ez4ted@users.noreply.github.com>
2018-10-24 14:12:59 -0600

    1024

--- creating a 2d array/sketch.js
+++ creating a 2d array/sketch.js
@@ -4,26 +4,57 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let rows = 5;
-let cols = 5;
+
+
+
+
+
+let rows = 12;
+let cols = 12;
 let grid;
 let cellSize;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cekkSuze = width / cols;
+  createCanvas(600, 600);
+  cellSize = width / cols;
   grid = createRandom2dArray(cols, rows);
 }
 
-function draw() {
+function draw() {background(255);
+  displayGrid();
+}
 
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill('red');
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function mousePressed() {
+  let x = floor(mouseX / cellSize);
+  let y = floor(mouseY / cellSize);
+
+  if (grid[y][x] === 1){
+    grid [y][x] = 0;
+  }
+  else if (grid[y][x] === 0){
+    grid [y][x] = 1;
+  }
 }
 
 function createRandom2dArray(cols, rows) {
   let randomGrid = [];
-  for (let x = 0; x < cols; x++) {
+  for (let y = 0; y < rows; y++) {
     randomGrid.push([]);
-    for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
       if (random(100) < 50) {
         randomGrid[y].push(0);
       }
@@ -33,4 +64,32 @@ function createRandom2dArray(cols, rows) {
     }
   }
   return randomGrid;
+}
+
+function deadOrAlive() {
+  let liveCounter;
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
+  if (grid[y-1][x-1] === 0) {
+    liveCounter ++;
+  }
 }
