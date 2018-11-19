659f7c5345648673a8e1b96f9d11c8b31e19cf16
2ez4ted <43014088+2ez4ted@users.noreply.github.com>
2018-11-19 14:13:55 -0600

    1119

--- bouncing balls/sketch.js
+++ bouncing balls/sketch.js
@@ -13,11 +13,17 @@ class Ball{
     this.dx = random(-10, 10);
     this.dy = random(-10, 10);
     this.color = color(random(255), random(255), random(255), 120);
+    this.isCollidingRightNow = false;
   }
 
   display() {
     noStroke();
-    fill(this.color);
+    if (this.isCollidingRightNow) {
+      fill("red");
+    }
+    else {
+      fill(this.color);
+    }
     ellipse(this.x, this.y, this.radius*2, this.radius*2);
   }
 
@@ -32,6 +38,22 @@ class Ball{
       this.dx *= -1;
     }
   }
+
+  checkForCollision(otherBall) {
+    // otherBall  = new Ball;
+    if (dist(this.x, this.y, otherBall.x, otherBall.y) <= this.radius + otherBall.radius) {
+      this.isCollidingRightNow = true;
+      // otherBall.isCollidingRightNow = true;
+      this.dx *= -1;
+      this.dy *= -1;
+      otherBall.dx *= -1;
+      otherBall.dx *= -1;
+    }
+    else {
+      this.isCollidingRightNow = false;
+      // otherBall.isCollidingRightNow = false;
+    }
+  }
 }
 
 let someBall;
@@ -44,10 +66,14 @@ function setup() {
 function draw() {
   background(0);
   for (let i = ballArray.length - 1; i >= 0; i--) {
+    for (let j = ballArray.length - 1; j >= 0; j --) {
+      if (i !== j) {
+        ballArray[i].checkForCollision(ballArray[j]);
+      }
+    }
     ballArray[i].update();
     ballArray[i].display();
   }
-
 }
 
 function mousePressed() {
---
+++ p5-1/sketch.js
@@ -0,0 +1,16 @@
+// snake
+// November 19th, 2018
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let gameStage = 0;
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
