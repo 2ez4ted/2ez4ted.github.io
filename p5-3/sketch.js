801b709216d7d52b23dc701cea40f880c64e3908
2ez4ted <43014088+2ez4ted@users.noreply.github.com>
2019-01-08 14:06:28 -0600

    1/8

--- p5-3/sketch.js
+++ p5-3/sketch.js
@@ -4,11 +4,21 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+class Card {
+  construction(cardColor, cardNumber) {
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+  }
 }
 
-function draw() {
+let cardColorList = ["club", "spade", "heart", "diamond"];
+let cardNumberList = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let hand = new Card;
 
+function setup() {
+  for (i = 0; i < cardColorList.length-1; i++) {
+    cardColor = cardColorList[i];
+    for (j = 0; j < cardNumberList.length-1; j++) {
+      cardNumber = cardNumberList[j];
+    }
+  }
 }
