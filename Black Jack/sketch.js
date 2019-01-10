7d93de69833f32273acd67548a768ff0b893bbb9
2ez4ted <43014088+2ez4ted@users.noreply.github.com>
2019-01-10 14:16:15 -0600

    110

--- Black Jack/sketch.js
+++ Black Jack/sketch.js
@@ -6,44 +6,50 @@
 // - describe what you did to take this project "above and beyond"
 class Card {
   construction(cardColor, cardNumber) {
-    this.aceDuality = false;
-
-    if (cardNumber > 10) {
-      cardNumber = 10;
-    }
-    else if (cardNumber === 1) {
-      this.aceDuality = true;
-    }
-
     this.cardColor = cardColor;
     this.cardNumber = cardNumber;
+    this.x = 100;
+    this.y = 100;
+
   }
 
-  createCard(cardColor, cardNumber)
+  display() {
+    noStroke();
+    fill(255);
+    rect(this.x, this.y, 20, 30);
+  }
 
 
 }
 
+let hand = [];
+let deck = [];
 let cardColorList = ["C", "S", "H", "D"]; //club, spade, heart and diamond
 let cardNumberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-let card = new Card;
 
 function setup() {
+  createCanvas(600, 600);
+  background("green");
+
+  addDeck();
+  addDeck();
+  addDeck();
+  addDeck();
 
 }
 
-function createDeck() {
-  deck = [];
+function draw() {
+  background("green");
+  deck[1].display();
+}
 
-  for (i = 0; i < 8; i++) {
-    for (j = 0; j < cardColorList.length; j++) {
-      for (v = 0; v < cardNumberList; v++) {
-        deck.push(card(j, v));
-      }
+
+
+function addDeck() {
+  for (let i = 0; i < cardColorList.length; i++) {
+    for (let j = 0; j < cardNumberList.length; j++) {
+      let someCard = new Card(i, j);
+      deck.push(someCard);
     }
   }
-}
-
-function shuffledeck() {
-  shuffle(deck);
 }
