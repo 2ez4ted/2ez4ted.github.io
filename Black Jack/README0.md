c691556e7668af5b6bfa384c742708a4f6a9ab24
2ez4ted <43014088+2ez4ted@users.noreply.github.com>
2019-01-11 14:08:35 -0600

    ohhhh

--- Black Jack/README0.md
+++ Black Jack/README0.md
@@ -1,4 +1,21 @@
 BlackJack for 1
+need to have:
+  1. Deck and cards. Deck is the essential component of Black Jack consists of multiple cards.
+  Cards are objects that carries different values which will essentially determine the result of
+  this game.
+  2. House. House is basically a virtue player that obeys and only obeys certain rules. The rules
+  it obeys are included in the rules section.
+  3. Asset. Asset consists of bet and net worth. Player will own certain virtue net that will be
+  represented by a value in the program. Player can set his own bet each turn.
+  4. Basic mechanisms. Mechanisms consist of deck setup, turn setup, decision making and win/loss
+  determinations.
+    Deck Setup: Setup of the deck with a given value of decks. The deck will shuffle and burn the first
+    card automatically as a costume of traditional black jack.
+    Turn Setup: Player will draw one card, then house will draw one.
+    Decision Making: Player will have a chance 
+
+
+
 
 
 House
--- Black Jack/sketch.js
+++ Black Jack/sketch.js
@@ -5,51 +5,57 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 class Card {
-  construction(cardColor, cardNumber) {
-    this.cardColor = cardColor;
+  constructor(cardSuit, cardNumber, x, y) {
+    this.cardColor = cardSuit;
     this.cardNumber = cardNumber;
-    this.x = 100;
-    this.y = 100;
+    this.x = x;
+    this.y = y;
+    this.xLength = 20;
+    this.yLength = 30;
 
   }
 
-  display() {
-    noStroke();
-    fill(255);
-    rect(this.x, this.y, 20, 30);
+  display(x, y) {
+    this.x = x;
+    this.y = y;
+    fill("white");
+    rect(this.x, this.y, this.xLength, this.yLength);
   }
-
-
 }
 
 let hand = [];
 let deck = [];
-let cardColorList = ["C", "S", "H", "D"]; //club, spade, heart and diamond
+let cardColorList = ["heart", "spade", "club", "diamond"]; //club, spade, heart and diamond
 let cardNumberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let someCard;
 
 function setup() {
   createCanvas(600, 600);
-  background("green");
 
-  addDeck();
-  addDeck();
-  addDeck();
-  addDeck();
-
+  addDeck(4);
+  burnDeck();
 }
 
 function draw() {
+
   background("green");
-  deck[1].display();
+
+  deck[0].display(1, 200);
+  rect(200, 200, 20, 30);
 }
 
-
-
-function addDeck() {
-  for (let i = 0; i < cardColorList.length; i++) {
-    for (let j = 0; j < cardNumberList.length; j++) {
-      let someCard = new Card(i, j);
-      deck.push(someCard);
+function addDeck(deckNumber) {
+  for (let x = 0; x < deckNumber; x++) {
+    for (let i = 0; i < cardColorList.length; i++) {
+      for (let j = 0; j < cardNumberList.length; j++) {
+        let someCard = new Card(cardColorList[i], cardNumberList[j]);
+        deck.push(someCard);
+      }
     }
   }
+}
+
+function burnDeck() {
+  shuffle(deck, true);
+  deck.splice(-1, 1);
 }
